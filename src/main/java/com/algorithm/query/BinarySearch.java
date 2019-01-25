6b1402aaf7051c6437c6b3f846a4bc10edddc7fd
wencaixu <710201236@qq.com>
2019-01-25 10:28:24 +0800

    二分法查找

--- src/main/java/com/algorithm/query/BinarySearch.java
+++ src/main/java/com/algorithm/query/BinarySearch.java
@@ -2,12 +2,10 @@ package com.algorithm.query;
 
 public class BinarySearch {
 
-    public <T extends Comparable> T search(T[] A, int low, int high, T n) {
+    public <T extends Comparable> int search(T[] A, int low, int high, T n) {
         int mid = (low + high) / 2;
 
-        if (low == high) {
-            return (T) Integer.valueOf(-1);
-        }
+        if (low == high) return -1;
 
         if (0 < A[mid].compareTo(n)) {
             high = mid - 1;
@@ -16,11 +14,11 @@ public class BinarySearch {
             low = mid + 1;
             return search(A, low, high, n);
         }
-        return A[mid];
+        return mid;
     }
 
     public static void main(String[] args) {
         Integer[] t = {1, 3, 2};
-        System.out.println(new BinarySearch().search(t, 1, 3, 2));
+        System.out.println(new BinarySearch().search(t, 0, 2, 3));
     }
 }
