875f636b9bb4a17662d1f35d8447a74a54d7aa2e
wencaixu <710201236@qq.com>
2019-01-25 14:06:13 +0800

    希尔排序

--- src/main/java/com/algorithm/sort/ShellSort.java
+++ src/main/java/com/algorithm/sort/ShellSort.java
@@ -44,7 +44,6 @@ public class ShellSort {
 
 
     public static void main(String[] args) {
-
         Double[] a = {1.4, 1.3, 1.5};
         new ShellSort().sortA(a);
         for (int i = 0; i < a.length; i++) {
